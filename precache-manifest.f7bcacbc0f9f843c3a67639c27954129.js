4e7147dbe22d6807760afb85e1a5946b478847cb
yasmin <yasminagwan98@gmail.com>
2019-12-01 22:08:43 +0530

    Updates

---
+++ precache-manifest.f7bcacbc0f9f843c3a67639c27954129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2505ad10ad2834a7089fb9780d74ccce",
+    "url": "/campaigns/index.html"
+  },
+  {
+    "revision": "f004d3f122a0dc4e704d",
+    "url": "/campaigns/static/css/main.14fb6f84.chunk.css"
+  },
+  {
+    "revision": "ff6a7d7ff8ab574fea41",
+    "url": "/campaigns/static/js/2.1e57ca02.chunk.js"
+  },
+  {
+    "revision": "f004d3f122a0dc4e704d",
+    "url": "/campaigns/static/js/main.ed2e3671.chunk.js"
+  },
+  {
+    "revision": "34023852513b571f24a0",
+    "url": "/campaigns/static/js/runtime-main.898ada2c.js"
+  }
+]);
